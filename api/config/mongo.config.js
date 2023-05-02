d54117dc6746a594dbe47315c5d870fb5e3090df
sagar poudel <iamsagar099@gmail.com>
2023-05-02 20:16:36 +0545

    back complete

--- api/config/mongo.config.js
+++ api/config/mongo.config.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const dbUrl= "mongodb://localhost:27017/stack-10"
+// const dbUrl= "mongodb://localhost:27017/stack-10"
+// const  dbUrl = process.env.MONGO_CON;
+const dbUrl = "mongodb+srv://iamsagar099:R7sEFE6PwcAvEPTv@cluster0.norvprb.mongodb.net/?retryWrites=true&w=majority"
+
 
 mongoose.connect(dbUrl,{
     autoCreate: true,
